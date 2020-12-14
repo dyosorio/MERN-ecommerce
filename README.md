0ccdd55bf7122af857f18dbf70dd426847637ce5
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-12-14 23:29:40 +0100

    created react router

--- README.md
+++ README.md
@@ -7,3 +7,15 @@
 2. create the header and footer components
 3. create Home page and product listing
    - create Product component
+4. Create Rating Component, import it to the Product component
+   - Rating takes two props: value and text
+   - import PropTypes to validate the prop types
+5. Implement React router
+   - npm i react-router-dom react-router-bootstrap
+   - import BrowserRouter as Router and wrap the entire app return inside <Router></Router>
+   - import Route to create routes to every page
+   - create Product page component, import it to App.js
+   - create a route for Home and another route for Product
+   - We'll change the <a> and use <Link> to avoid the page to reload every time we click on a <a>. This is a SPA, no need to reload
+   - Go to Products.js and import { Link } from react-router-dom. Link takes a to={} attribute instead of an href
+   - Header.js we're not going to use <Link>, for this we're going to import { LinkContainer } from react-router-bootstrap which does the same as <Link>, it allows us to wrap bootstrap components.Header.js wrap the nav links inside the <NavContainer>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,16 +7,22 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>The Shop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>The Shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i>cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i>user
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i>cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i>user
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
